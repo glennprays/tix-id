130f87117b7c7f203a40206a1caab72e7dac4b90
glennprays <glennsagala@gmail.com>
2023-04-25 05:34:21 +0700

    fix: cofig db connection with .env

--- config/db.go
+++ config/db.go
@@ -7,7 +7,7 @@ import (
 )
 
 func connect() *sql.DB {
-	db, err := sql.Open("mysql", "root:@tcp("+os.Getenv("DB_ADDRESS")+":3306)/"+os.Getenv("DB_TABLE_NAME")+"?parseTime=true&loc=Asia%2FJakarta")
+	db, err := sql.Open("mysql", os.Getenv("DB_USER")+":"+os.Getenv("DB_PASSWORD")+"@tcp("+os.Getenv("DB_HOST")+":"+os.Getenv("DB_PORT")+")/"+os.Getenv("DB_NAME")+"?parseTime=true&loc=Asia%2FJakarta")
 
 	if err != nil {
 		log.Fatal(err)
