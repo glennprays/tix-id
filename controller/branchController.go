264f536a624ea812e2e4139766977a2e4a473113
JimmyFXW <114449647+JimmyFXW@users.noreply.github.com>
2023-04-26 21:45:16 +0700

    initial commit
    get all branches endpoint

--- controller/branchController.go
+++ controller/branchController.go
@@ -1,7 +1,9 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
+	"tix-id/config"
 	"tix-id/models"
 
 	"github.com/gin-gonic/gin"
@@ -42,12 +44,61 @@ func CreateBranch(c *gin.Context) {
 // @Success 200 {object} models.BranchesResponse
 // @Router /branches [get]
 func GetBranches(c *gin.Context) {
-	// branchId := c.Query("branchId")
+	db := config.ConnectDB()
+
+	// Ensure the database connection is closed when the function returns
+	defer db.Close()
+
+	// Execute a SELECT query to retrieve all branches from the database
+	rows, err := db.Query("SELECT * FROM branch")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve branches from database"})
+		return
+	}
+
+	// Iterate over the rows returned from the query and store them in a slice of Branch structs
 	var branches []models.Branch
+	for rows.Next() {
+		var branch models.Branch
+		err := rows.Scan(&branch.ID, &branch.Name, &branch.Address)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve branches from database"})
+			return
+		}
+
+		// Execute a SELECT query to retrieve all theatres for the current branch
+		theatreRows, err := db.Query("SELECT id, name FROM theatre WHERE branch_id = ?", branch.ID)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error1": err})
+			return
+		}
+
+		// Create an empty slice of Theatre structs to hold the retrieved theatres
+		var theatres []models.Theatre
+
+		// Iterate over the theatre rows returned from the query and store them in the theatres slice
+		for theatreRows.Next() {
+			var theatre models.Theatre
+			err := theatreRows.Scan(&theatre.ID, &theatre.Name)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error2": fmt.Sprintf("%v", err)})
+				return
+			}
+			theatres = append(theatres, theatre)
+		}
+
+		// Add the retrieved theatres to the current branch
+		branch.Theatres = &theatres
+
+		// Add the current branch to the branches slice
+		branches = append(branches, branch)
+	}
+
+	// Create a BranchesResponse struct with the retrieved branches and send it as a JSON response
 	responseData := models.BranchesResponse{
 		Response: models.Response{
 			Status:  200,
-			Message: "Branch updated successfully",
+			Message: "Branches retrieved successfully",
 		},
 		Branches: branches,
 	}
